6e94e24cb59206ac71ee01a38dcc7c6eacd2b152
juliancardz <lion11@videotron.ca>
2013-04-10 22:00:00 -0300

    Change 2

--- test..js
+++ test..js
@@ -1,6 +1,1 @@
-javascript: (function () {
-			 var bot = document.createElement('script');
-			 bot.setAttribute('id', 'RblxBot');
-			 bot.setAttribute('src', 'https://raw.github.com/connergdavis/Plugbot/master/plugbot.js');
-			 document.body.appendChild(jsCode);
-}());
+Test
